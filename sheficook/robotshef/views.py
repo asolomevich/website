78f1f3f574ca1b2a48046ee8fc166ada552a22a5
Arina Solomevich <jones-04@list.ru>
2023-10-30 20:20:15 +0300

    first commit

---
+++ sheficook/robotshef/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'robotshef/index.html')
+
+def about(request):
+    return render(request, 'robotshef/about.html')
